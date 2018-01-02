45f39f21f219147111ffad335f00ac7ef037fdc7
527lin <1134580715@qq.com>
2018-01-02 16:58:35 +0800

    kihlhk

---
+++ src/cn/xing/gfg.java
@@ -0,0 +1,20 @@
+package cn.xing;
+
+import java.util.Scanner;
+
+public class gfg {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+//Ctrl+AlIt+O 导包
+	Scanner sc=new Scanner(System.in);
+	System.out.println("请输入一个数");
+	int a=sc.nextInt();
+	 int[] arr=new int[a];
+	System.out.println("数组长度为");
+	System.out.println(arr.length);
+	}
+	
+	
+
+}
